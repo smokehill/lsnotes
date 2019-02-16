12abebe005046969942b8573be167ba078849151
smokehill <valera.padolochniy@gmail.com>
2019-02-16 18:32:12 +0300

    settings export/import

--- build/js/components/content/settings.vue
+++ build/js/components/content/settings.vue
@@ -4,13 +4,34 @@
       <h4 class="title">Settings</h4>
     </div>
     <div class="content-body">
-        <div class="storage-info">
-          <span class="storage-info-title">Local Storage</span>
-          <span class="storage-info-text"><b>Total:</b> {{ storage.total }} MB, <b>Usage:</b> {{ storage.used }} MB</span>
-          <div class="storage-info-progress">
-            <div class="storage-info-progress-bar" :style="'width:'+storage.progress+'%'"></div>
-          </div>
+      <div class="storage-info">
+        <span class="storage-info-title">Local Storage</span>
+        <span class="storage-info-text"><b>Total:</b> {{ storage.total }} MB, <b>Usage:</b> {{ storage.used }} MB</span>
+        <div class="storage-info-progress">
+          <div class="storage-info-progress-bar" :style="'width:'+storage.progress+'%'"></div>
         </div>
+      </div>
+      <div class="storage-management">
+        <ul>
+          <li>
+            <a href="#" id="export-btn">
+              <img src="dist/img/icons/settings_storage_export.png" alt="export" />
+              <span>Export</span>
+            </a>
+          </li>
+          <li>
+            <div class="import-block">
+              <label for="import-file" id="import-btn">
+                <img src="dist/img/icons/settings_storage_import.png" alt="import" />
+                <span>Import</span>
+              </label>
+              <input type="file" name="photo" id="import-file" accept="application/json" v-import-file />
+              <a href="#" id="import-start-btn" v-on:click="importNotes($event)">Go</a>
+              <span>No file</span>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -33,6 +54,29 @@
     mounted: function() {
       this.$parent.highlightMenu();
       this.initStorageInfo();
+      this.initExport();
+    },
+    directives: {
+      'import-file': {
+        update: (el, binding, vnode) => {
+          $(el).on('change', function() {
+            let reader = new FileReader();
+            let file = $(this)[0].files[0];
+            if (file.type == 'application/json') {
+              reader.onload = function() {
+                let data = JSON.parse(reader.result);
+                console.log(data);
+                // TODO validate data
+                // TODO make import
+              };
+              reader.readAsText(file);
+            } else {
+              // TODO disiplay error
+              console.log('Error. Incorrect file format.');
+            }
+          });
+        }
+      }
     },
     methods: {
       /**
@@ -43,6 +87,23 @@
         this.storage.total = 10;
         this.storage.used = this.__calcUsedSpace();
         this.storage.progress = this.__calcProgress();
+      },
+      /**
+       * Export notes from LS into json file
+       */
+      initExport() {
+        let lsNotes = JSON.stringify(lsGet('notes'));
+        // save the file contents as a DataURI and write it as the href for the link
+        $('#export-btn').attr('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(lsNotes));
+        $('#export-btn').attr('download', 'ms_notes.json');
+      },
+      /**
+       * Import notes into LS from json file
+       */
+      importNotes(e) {
+        e.preventDefault();
+        console.log('import');
+        // TODO
       },
       /**
        * @internal
